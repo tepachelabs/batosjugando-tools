c2e042a4dacc8478fe0b1f54b5953bfb4c08d0e9
Jose Salcido <jmsalcidoaguilar@gmail.com>
2020-06-26 12:18:21 -0700

    upgrade rubocop to not check schema

--- backend-rails/db/schema.rb
+++ backend-rails/db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_185647) do
-
+ActiveRecord::Schema.define(version: 20_200_626_185_647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,9 +23,9 @@ ActiveRecord::Schema.define(version: 2020_06_26_185647) do
     t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index %w[author_type author_id], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+    t.index %w[resource_type resource_id], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
   create_table "admin_users", force: :cascade do |t|
@@ -74,8 +73,8 @@ ActiveRecord::Schema.define(version: 2020_06_26_185647) do
     t.index ["admin_user_id"], name: "index_publish_configurations_on_admin_user_id"
   end
 
-# Could not dump table "publish_jobs" because of following StandardError
-#   Unknown type 'published_job_status' for column 'status'
+  # Could not dump table "publish_jobs" because of following StandardError
+  #   Unknown type 'published_job_status' for column 'status'
 
   add_foreign_key "publish_configurations", "admin_users"
   add_foreign_key "publish_jobs", "podcast_episodes", on_delete: :cascade
