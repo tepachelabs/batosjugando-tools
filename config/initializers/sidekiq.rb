754bf9672efc1bd9aebed53628b3450a5f3451d1
Jose Salcido <jmsalcidoaguilar@gmail.com>
2023-03-26 13:15:36 -0700

    cconfigure

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,14 @@
-schedule_file = 'config/schedule.yml'
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: ENV.fetch("REDIS_HOST", 'redis://localhost:6379'),
+    password: ENV.fetch("REDIS_PASSWORD", 'password')
+  }
 
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    config.redis = {
-      url: ENV.fetch("REDIS_HOST", 'redis://localhost:6379'),
-      password: ENV.fetch("REDIS_PASSWORD", 'password')
-    }
+  config.on(:startup) do
+    schedule_file = "config/users_schedule.yml"
+
+    if File.exist?(schedule_file)
+      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+    end
   end
 end
-
-Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file) && Rails.env.production?
