96c234356d4dfd9788ee834f723df8ed473f2017
Jose Salcido <jmsalcidoaguilar@gmail.com>
2023-03-26 12:40:57 -0700

    if else

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,11 @@
 schedule_file = 'config/schedule.yml'
 
-if Rails.env.production? do
+if Rails.env.production?
   Sidekiq.configure_client do |config|
     config.redis = {
       url: ENV.fetch("REDIS_HOST", 'redis://localhost:6379'),
       password: ENV.fetch("REDIS_PASSWORD", 'password')
     }
-    end
   end
 end
 
